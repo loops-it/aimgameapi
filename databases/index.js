09f69baea713b567aab8f15d61e089c5961efc22
tj <thinupavani@gmail.com>
2023-12-27 15:52:05 +0530

    db config update

--- databases/index.js
+++ databases/index.js
@@ -4,9 +4,10 @@ const { database } = require("../config");
 module.exports.connect = async () => {
   try {
     const data = await mongoose.connect(
-      `mongodb+srv://${database.user}:${database.password}@${database.host}/${database.database}?retryWrites=true&w=majority`
+       `mongodb+srv://yohan:123admin@cluster0.o4rpy.mongodb.net/aimgame-stging?retryWrites=true&w=majority`
+      // `mongodb://localhost:27017/admin`
     );
-    console.log(`-----Connected to ${database.database} successfully-----`);
+    console.log(`-----Connected to aimgame-stging successfully-----`);
   } catch (error) {
     console.log("db connection error ", { error });
   }
