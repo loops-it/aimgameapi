084e63a12d356595413f5e59258e2d8cec14212d
tj <thinupavani@gmail.com>
2023-12-27 17:08:50 +0530

    update

--- databases/index.js
+++ databases/index.js
@@ -4,11 +4,20 @@ const { database } = require("../config");
 module.exports.connect = async () => {
   try {
     const data = await mongoose.connect(
-       `mongodb+srv://yohan:123admin@cluster0.o4rpy.mongodb.net/aimgame-stging?retryWrites=true&w=majority`
-      // `mongodb://localhost:27017/admin`
+      `mongodb+srv://${database.user}:${database.password}@${database.host}/${database.database}?retryWrites=true&w=majority`
     );
-    console.log(`-----Connected to aimgame-stging successfully-----`);
+    console.log(`-----Connected to ${database.database} successfully-----`);
   } catch (error) {
     console.log("db connection error ", { error });
   }
+
+  // try {
+  //   const data = await mongoose.connect(
+  //      `mongodb+srv://yohan:123admin@cluster0.o4rpy.mongodb.net/aimgame-stging?retryWrites=true&w=majority`
+  //     // `mongodb://localhost:27017/admin`
+  //   );
+  //   console.log(`-----Connected to aimgame-stging successfully-----`);
+  // } catch (error) {
+  //   console.log("db connection error ", { error });
+  // }
 };
