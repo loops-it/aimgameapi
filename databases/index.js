140e09711c9d43eddc645ef01f01c3d6b6fdaa9b
tj <thinupavani@gmail.com>
2023-12-28 16:00:24 +0530

    update

--- databases/index.js
+++ databases/index.js
@@ -10,14 +10,4 @@ module.exports.connect = async () => {
   } catch (error) {
     console.log("db connection error ", { error });
   }
-
-  // try {
-  //   const data = await mongoose.connect(
-  //      `mongodb+srv://yohan:123admin@cluster0.o4rpy.mongodb.net/aimgame-stging?retryWrites=true&w=majority`
-  //     // `mongodb://localhost:27017/admin`
-  //   );
-  //   console.log(`-----Connected to aimgame-stging successfully-----`);
-  // } catch (error) {
-  //   console.log("db connection error ", { error });
-  // }
 };
